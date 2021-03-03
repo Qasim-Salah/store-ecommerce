3284fbce964b03b38e7364c6a6d743715e26f76d
qasem-salah <qasimsalah612@gmail.com>
2021-03-03 18:42:35 +0300

    =product

---
+++ app/Models/Product.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Models;
+
+use Astrotomic\Translatable\Translatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use Translatable, SoftDeletes;
+
+    protected $with = ['translations'];
+
+    protected $guarded = [];
+
+    protected $casts = ['manage_stock' => 'boolean', 'in_stock' => 'boolean', 'is_active' => 'boolean',];
+
+    protected $dates = ['special_price_start', 'special_price_end', 'start_date', 'end_date', 'deleted_at',];
+
+    protected $translatedAttributes = ['name', 'description', 'short_description'];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class)->withDefault();
+    }
+
+    public function getActive()
+    {
+        return $this->is_active == 0 ? 'غير مفعل' : 'مفعل';
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'product_categories');
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->where('is_active', 1);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'product_tags');
+    }
+
+    public function options()
+    {
+        return $this->hasMany(Option::class, 'product_id');
+    }
+
+    public function images()
+    {
+        return $this->hasMany(Image::class, 'product_id');
+    }
+
+    public function hasStock($quantity)
+    {
+        return $this->qty >= $quantity;
+    }
+
+    public function outOfStock()
+    {
+        return $this->qty === 0;
+    }
+
+    public function inStock()
+    {
+        return $this->qty >= 1;
+    }
+
+    public function getTotal($converted = true)
+    {
+        return $total = $this->special_price ?? $this->price;
+
+    }
+
+}
